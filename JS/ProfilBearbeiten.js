c4d182afca29de148c06ce63afeac3212fcd7058
Jonathan Vielwerth <jonathanvielwerth@gmail.com>
2023-06-29 15:28:51 +0200

    Script tag richtig und bissel kommentare

--- JS/ProfilBearbeiten.js
+++ JS/ProfilBearbeiten.js
@@ -1,6 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", init)
+
+function init(){
 var ProfiButton = document.getElementById("ProfiAnfrageButton");
 ProfiButton.addEventListener("click", alertProfi);
-
+}
 
 function alertProfi(){
 	alert ("Profi Anfrage wurde erfolgreich verschickt  TEST 2 ");
